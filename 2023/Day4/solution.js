53fe0c4a54ca75fa3cfd41d54e277600443080c0
Tim Roty <42622148+timroty@users.noreply.github.com>
2023-12-15 09:27:15 -0600

    Day 4 Solution

--- 2023/Day4/solution.js
+++ 2023/Day4/solution.js
@@ -36,41 +36,14 @@ function processWinningNumbers_1(data) {
   return total_points;
 }
 
+// Could combine one and two very easily, but oh well
 function processWinningNumbers_2(data) {
   const lines = data.split("\n");
 
-  const memo_table = {};
-  const queue = [...Array(lines.length).keys()];
+  const count_array = new Array(lines.length).fill(1);
 
-  let queue_shift = queue.shift();
-
-  while (!isNaN(queue_shift)) {
-    const winner_count = getWinnerCount(queue_shift);
-
-    for (let j = 1; j <= winner_count; j++) {
-      queue.push(Number(queue_shift + j));
-    }
-
-    queue_shift = queue.shift();
-  }
-
-  // Ideally I don't loop over again ... but I've
-  // spent too long on the problem today
-  let sum = 0;
-  for (const [card] of Object.entries(memo_table)) {
-    sum += memo_table[card].times;
-  }
-
-  return sum;
-
-  // Helper Function
-  function getWinnerCount(input_number) {
-    if (memo_table[input_number]) {
-      memo_table[input_number].times = memo_table[input_number].times + 1;
-      return memo_table[input_number].winners;
-    }
-
-    const all_numbers = lines[input_number].split(":")[1].trim();
+  for (let i = 0; i < lines.length; i++) {
+    const all_numbers = lines[i].split(":")[1].trim();
     const split_numbers = all_numbers.split("|");
     const winning_numbers = new Set(
       split_numbers[0].trim().replace(/\s+/g, " ").split(" ")
@@ -80,17 +53,19 @@ function processWinningNumbers_2(data) {
       .replace(/\s+/g, " ")
       .split(" ");
 
-    let number_of_winners = 0;
+    let winning_card_count = 0;
     for (let j = 0; j < card_numbers.length; j++) {
       if (winning_numbers.has(card_numbers[j])) {
-        number_of_winners++;
+        winning_card_count++;
       }
     }
 
-    memo_table[input_number] = { winners: number_of_winners };
-    memo_table[input_number].times = 1;
-    return number_of_winners;
+    for (let j = 1; j <= winning_card_count; j++) {
+      count_array[i + j] += count_array[i];
+    }
   }
+
+  return count_array.reduce((a, b) => a + b, 0);
 }
 
 exports.processWinningNumbers_1 = processWinningNumbers_1;
